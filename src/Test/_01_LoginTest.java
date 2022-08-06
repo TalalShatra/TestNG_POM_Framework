5fb25ace645381c3055817b010cc01540abf1244
talalshatra@yahoo.com <talalshatra1966>
2022-08-06 14:24:01 -0400

    Initial commit

---
+++ src/Test/_01_LoginTest.java
@@ -0,0 +1,58 @@
+package Test;
+
+import POM.HomePageElements;
+import POM.LoginPageElements;
+import POM.MyAccountPageElements;
+import Utils.BaseDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class _01_LoginTest extends BaseDriver {
+
+    HomePageElements homePageElements;
+    LoginPageElements loginPageElements;
+    MyAccountPageElements myAccountPageElements;
+
+
+    @Test
+    public void LoginPositiveTest(){
+
+        homePageElements = new HomePageElements(driver);
+        loginPageElements = new LoginPageElements(driver);
+        myAccountPageElements = new MyAccountPageElements(driver);
+
+        homePageElements.myAccountButton.click();
+        homePageElements.loginButton.click();
+
+        loginPageElements.emailInput.sendKeys("wellsa@gmail.com");
+        loginPageElements.passwordInput.sendKeys("soso1971");
+        loginPageElements.loginButton.click();
+
+        String myAccountHeaderText = myAccountPageElements.myAccountHeader.getText();
+
+        Assert.assertEquals(myAccountHeaderText, "My Account");
+    }
+    @Test
+    public void LoginNegativeTest(){
+
+        homePageElements = new HomePageElements(driver);
+        loginPageElements = new LoginPageElements(driver);
+
+        homePageElements.myAccountButton.click();
+        homePageElements.loginButton.click();
+
+        loginPageElements.emailInput.sendKeys("wellsa@gmail.com");
+        loginPageElements.passwordInput.sendKeys("12345");
+        loginPageElements.loginButton.click();
+
+
+        String loginFailedMessageText = loginPageElements.loginFailedMessage.getText();
+        boolean isErrorDisplayed = loginPageElements.loginFailedMessage.isDisplayed();
+
+        Assert.assertTrue(isErrorDisplayed);
+
+        Assert.assertEquals(loginFailedMessageText, "Warning: No match for E-Mail Address and/or Password.");
+    }
+
+
+}
